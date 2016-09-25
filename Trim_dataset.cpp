3b46542702e6d4db65a7cd84c349298b44ae653c
syotaKUSHIZAKI <syota.kushizaki@gmail.com>
2016-09-25 19:56:20 +0900

    trim dataset
    ver2

--- Trim_dataset.cpp
+++ Trim_dataset.cpp
@@ -2,13 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <opencv_lib.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 #pragma warning(disable:4996)
 using namespace std;
+using namespace cv;
 
 std::vector<std::string> split(std::string str, std::string delim) {
 	std::vector<std::string> items;
@@ -33,24 +38,56 @@ std::vector<std::string> split(std::string str, std::string delim) {
 
 int main()
 {
-	string filename = "torsobox.csv";
+	//string filename = "torsobox.csv";
+	string filename = "joints.csv";
+	string Path_dataset = "C:\\Users\\Shun\\Pictures\\DeepLearning\\dataset\\FLIC\\images\\";
+	string Path_output = "C:\\Users\\Shun\\Pictures\\DeepLearning\\dataset\\FLIC\\trim_images\\";
 	ifstream ifs(filename);
 
 	string buff;
 	string filepath;
-	int data[4];
+	int data[100] = {0};
 	getline(ifs, buff);
 	while (getline(ifs, buff)) {
+
+		//------------ load csv ---------------
 		vector<string> result = split(buff, ",");
 		for (int k = 0; k < result.size(); k++) {
 			if (k == 0) filepath = result[k];
 			else data[k - 1] = stoi(result[k]);
+			printf("data[%d] : ", k);
+			isnan
+			if (result[k])) cout << "NaN" << endl;
+			else cout <<  result[k] << endl;
+			getchar();
 		}
-		cout << "y" << filepath << "z" << " [" << data[0] << " " << data[1] << "] [" << data[2] << " " << data[3] << "]" << endl;
-		getchar();
+		//cout << "y" << filepath << "z" << " [" << data[0] << " " << data[1] << "] [" << data[2] << " " << data[3] << "]" << endl;
+
+		//-------------------------------------
+
+		Mat src_org = imread(Path_dataset + filepath);
+		//rectangle(src_org, Point(data[0], data[1]), Point(data[2], data[3]),Scalar(255,0,0),3,4);
+		int tb_width = abs(data[0] - data[2]);
+		int tb_height = abs(data[1] - data[3]);
+
+		const double k = 0.80;
+
+		if (data[0] - tb_width*k < 0) data[0] = 0;
+		else data[0] -= tb_width*k;
+		if (data[1] - tb_height*k < 0) data[1] = 0;
+		else data[1] -= tb_height*k;
+		if (data[2] + tb_width*k > src_org.cols) data[2] = src_org.cols;
+		else data[2] += tb_width*k;
+		if (data[3] + tb_height*k > src_org.rows) data[3] = src_org.rows;
+		else data[3] += tb_height*k;
+
+		//printf("[torsobox] = [ %d , %d ] , [ %d , %d ]\n", data[0], data[1], data[2], data[3]);
+
+		Mat src_roi  = src_org( Rect(data[0], data[1], data[2]-data[0], data[3]-data[1]));
+		imwrite(Path_output + filepath, src_roi);
 	}
 
-    return 0;
+	return 0;
 }
 
 
